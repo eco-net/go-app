8ccfca9772d93451687069c25e6083b1d48a73b6
Thomas Elvin <info@datalaere.dk>
2019-07-07 17:07:44 +0200

    Theme
    Added custom theme etc.

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,97 @@
+
+function fetch_and_display_posts(limit)
+{
+
+       $.get(settings.api + "?per_page=" + limit, function(data, status){
+        if(status == 'success') {
+
+           var posts = JSON.parse(JSON.stringify(data));
+
+            var html = "";
+
+            for(var count = 0; count < posts.length; count++)
+            {
+                var title = posts[count]['title']['rendered'];
+                var link = posts[count]['link'];
+                var date = posts[count]['date'];
+                var image = posts[count]['images']['medium'];
+
+                html = html + "<li>" + "<a href='javascript:open_browser(\"" + link + "\")'>" + "<img height='128' width='128' src='" + image + "'>" + "<h2>" + title + "</h2>" + "<p>" + date + "</p></a></li>";
+            }
+
+            document.getElementById("posts").innerHTML = html;
+
+            $('#post-page').bind('pageinit', function() {
+                $("#posts").listview("refresh");
+            });
+
+        } else {
+            alert('error');
+        }
+    });
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://grontoverblik.dk/wp-json/wp/v2/go/?per_page=1");
+    xhr.onload = function(){
+        var posts_array = JSON.parse(xhr.responseText);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var html = "";
 
-        console.log('Received Event: ' + id);
+        for(var count = 0; count < posts_array.length; count++)
+        {
+                    console.log(posts_array[count]);
+            var title = posts_array[count]['title']['rendered'];
+            var link = posts_array[count]['link'];
+            var date = posts_array[count]['date'];
+            var image = posts_array[count]['images']['medium'];
+
+            html = html + "<li>" + "<a href='javascript:open_browser(\"" + link + "\")'>" + "<img height='128' width='128' src='" + image + "'>" + "<h2>" + title + "</h2>" + "<p>" + date + "</p></a></li>";
+        }
+
+        document.getElementById("posts").innerHTML = html;
+        $('#post-page').bind('pageinit', function() {
+            $("#posts").listview("refresh");
+        });
     }
-};
+    xhr.send();
+    */
+}
+
+function login()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "")
+    {
+        navigator.notification.alert("Please enter username", null, "Username Missing", "OK");
+        return;
+    }
+
+    if(password == "")
+    {
+        navigator.notification.alert("Please enter password", null, "Password Missing", "OK");  
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost/wp-admin/admin-ajax.php?action=login&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password));
+    xhr.onload = function(){
+        if(xhr.responseText == "FALSE")
+        {
+            navigator.notification.alert("Wrong Username and Password", null, "Wrong Creds", "Try Again");
+        }
+        else if(xhr.responseText == "TRUE")
+        {
+            fetch_and_display_posts();
+            $("#page_two_link").click();
+        }
+    }   
+    xhr.send();
+}
+
+function open_browser(link)
+{
+    window.open(link, '_blank', 'location=yes');
+}
